3180ac0c7af667746b8da318e9a27498a33572e4
Kaciras <Kaciras@protonmail.com>
2023-11-05 14:58:25 +0800

    validate `defineSuite` is imported from ESBench

--- src/main/java/com/kaciras/esbench/RunLineMarker.java
+++ src/main/java/com/kaciras/esbench/RunLineMarker.java
@@ -3,27 +3,44 @@ package com.kaciras.esbench;
 import com.intellij.execution.PsiLocation;
 import com.intellij.execution.lineMarker.RunLineMarkerContributor;
 import com.intellij.icons.AllIcons;
+import com.intellij.lang.ecmascript6.psi.impl.ES6ImportPsiUtil;
 import com.intellij.lang.javascript.psi.JSCallExpression;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Arrays;
+
 public class RunLineMarker extends RunLineMarkerContributor {
+
+	private static String MOD = "\"@esbench/core/client\"";
+	private static String DEFINITION = "defineSuite";
 
 	@Override
 	public @Nullable Info getInfo(@NotNull PsiElement element) {
 		if (!(element instanceof JSCallExpression call)) {
 			return null;
 		}
-		if (!call.getMethodExpression().getText().equals("defineSuite")) {
+		if (!call.getMethodExpression().textMatches(DEFINITION)) {
 			return null;
 		}
-		var children = call.getContainingFile().getChildren();
+		if (!hasImportFromESBench(element.getContainingFile())) {
+			return null;
+		}
 
 		var location = new PsiLocation(call);
 		var name = location.getVirtualFile().getPath();
 		var action = new ESBenchAction(name);
 		return new Info(AllIcons.RunConfigurations.TestState.Run, new AnAction[]{action}, null);
+	}
+
+	private boolean hasImportFromESBench(PsiFile file) {
+		return ES6ImportPsiUtil.getImportDeclarations(file)
+				.stream()
+				.filter(i -> MOD.equals(i.getFromClause().getReferenceText()))
+				.flatMap(i -> Arrays.stream(i.getImportSpecifiers()))
+				.anyMatch(s -> s.textMatches(DEFINITION));
 	}
 }
