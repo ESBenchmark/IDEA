f8f006e472e03e28ce620ad954bd60b50b1790c8
Kaciras <Kaciras@protonmail.com>
2024-07-16 09:06:02 +0800

    use logo for run config icon

--- src/main/java/com/kaciras/esbench/ESBenchConfigType.java
+++ src/main/java/com/kaciras/esbench/ESBenchConfigType.java
@@ -4,10 +4,11 @@ import com.intellij.execution.configurations.RunConfiguration;
 import com.intellij.execution.configurations.RunConfigurationSingletonPolicy;
 import com.intellij.execution.configurations.SimpleConfigurationType;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.IconLoader;
 import com.intellij.openapi.util.NotNullLazyValue;
 import org.jetbrains.annotations.NotNull;
 
-import static com.intellij.icons.AllIcons.Actions.ProfileYellow;
+import javax.swing.*;
 
 public final class ESBenchConfigType extends SimpleConfigurationType {
 
@@ -15,7 +16,11 @@ public final class ESBenchConfigType extends SimpleConfigurationType {
 	public static final String NAME = "ESBench";
 
 	public ESBenchConfigType() {
-		super(ID, NAME, null, NotNullLazyValue.lazy(() -> ProfileYellow));
+		super(ID, NAME, null, NotNullLazyValue.lazy(ESBenchConfigType::pluginIcon));
+	}
+
+	private static Icon pluginIcon() {
+		return IconLoader.getIcon("/icons/ESBench.svg", ESBenchConfigType.class);
 	}
 
 	@NotNull
